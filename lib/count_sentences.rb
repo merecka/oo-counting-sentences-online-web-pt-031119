db40d977ddcd66e023002d00eabcc07b3d312d29
Alex Merecka <merecka@gmail.com>
2019-04-01 03:59:42 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,10 +16,7 @@ class String
 
   def count_sentences
     split_array = []
-    split_array = self.split(/\A\w+\W+/)
-  #  split_array = self.split(/\W+\s/)
-  #   split_array = self.split(/\W+/)
-    binding.pry
+    split_array = self.split(" ")
     split_array.count { |x| x.end_with?(".","?", "!")}
   end
 end
